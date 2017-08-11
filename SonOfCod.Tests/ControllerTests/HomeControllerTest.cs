e4ee622ebdc94376c7651e8adbeca50e35d95ebf
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-08-11 16:55:42 -0700

    test that the homecontroller is returning a view

---
+++ SonOfCod.Tests/ControllerTests/HomeControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SonOfCod.Controllers;
+using SonOfCod.Models;
+using Xunit;
+
+namespace SonOfCod.Tests.ControllerTests
+{
+    public class HomeControllerTest
+    {
+        private readonly AppDbContext _db;
+
+        [Fact]
+        public void Get_ViewResult_Index()
+        {
+            HomeController controller = new HomeController(_db);
+
+            IActionResult result = controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
--- src/SonOfCod/Controllers/HomeController.cs
+++ src/SonOfCod/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SonOfCod.Models;
 
 namespace SonOfCod.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext _db;
+
+        public HomeController(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
