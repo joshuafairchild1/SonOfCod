cc14d7d0c780b173701d3acc451b9cc1617f9c91
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-08-11 11:35:49 -0700

    add initial database migration

---
+++ src/SonOfCod/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SonOfCod.Models
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+    }
+}
